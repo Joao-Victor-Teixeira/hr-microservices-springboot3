3a96c1dd54c54a8bac94c9ca5e9fc4c1bec38b66
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-11-14 12:11:04 -0300

    Registra worker e payroll como clientes Eureka
    Adiciona a dependência \eureka-client` nos projetos hr-worker e hr-payroll.

--- hr-worker/src/main/java/com/joaodev/hr_worker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/joaodev/hr_worker/HrWorkerApplication.java
@@ -2,12 +2,13 @@ package com.joaodev.hr_worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient 
 @SpringBootApplication
 public class HrWorkerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrWorkerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HrWorkerApplication.class, args);
+    }
 }
--- hr_payroll/src/main/java/com/example/joaodev/hr_payroll/HrPayrollApplication.java
+++ hr_payroll/src/main/java/com/example/joaodev/hr_payroll/HrPayrollApplication.java
@@ -2,14 +2,15 @@ package com.example.joaodev.hr_payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient 
+@EnableFeignClients 
 @SpringBootApplication
-@EnableFeignClients
 public class HrPayrollApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrPayrollApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(HrPayrollApplication.class, args);
+    }
 }
