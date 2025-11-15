d9981c4914ac4086c155b20dd4a17e957005bce1
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-11-15 17:22:50 -0300

    implementa cliente do Config Server (hr-worker)
    Substitui o `bootstrap.properties` (legado) pela propriedade moderna `spring.config.import` no `application.properties`.
    
    Adiciona endpoint de teste `/configs` e `Thread.sleep` para preparar para as aulas de Actuator e Resilience4j.

--- hr-worker/src/main/java/com/joaodev/hr_worker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/joaodev/hr_worker/resources/WorkerResource.java
@@ -2,7 +2,11 @@ package com.joaodev.hr_worker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +20,22 @@ import com.joaodev.hr_worker.repositories.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Value("${test.config}")
+    private String testConfig;
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository repository;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<String> getConfigs(){
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.ok(testConfig);
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
@@ -27,6 +45,14 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+        }
+
+        logger.info("PORT = " + env.getProperty("local.server.port"));
         return repository.findById(id)
             .map(worker -> ResponseEntity.ok().body(worker))
             .orElse(ResponseEntity.notFound().build());
