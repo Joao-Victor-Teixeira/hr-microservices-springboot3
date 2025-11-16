e752ee761e6218593e675b9563cdec2c0a151f6a
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-11-16 08:49:49 -0300

    Renomeia pasta wr_payroll para wr-payroll
    Afim de manter o padrão de nomes

---
+++ hr-gateway/src/test/java/com/joaodev/hr_gateway/HrGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.hr_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
