56d709d07902f0a48a8b872a8e7be1481e395117
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-11-16 09:28:46 -0300

    implementa API Gateway com Spring Cloud Gateway

--- hr-gateway/src/main/java/com/joaodev/hr_gateway/HrGatewayApplication.java
+++ hr-gateway/src/main/java/com/joaodev/hr_gateway/HrGatewayApplication.java
@@ -2,7 +2,9 @@ package com.joaodev.hr_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HrGatewayApplication {
 
