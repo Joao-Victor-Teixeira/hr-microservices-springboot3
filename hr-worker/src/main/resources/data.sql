37d70cbea531641c7addb2a6ca1d369630be1323
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-11-19 17:48:57 -0300

    Migra persistência para PostgreSQL e adiciona Dockerfiles
    Dockerização:
    
    Criação de Dockerfile (Multistage Build com eclipse-temurin:21-alpine) para todos os 7 microsserviços (eureka, config, gateway, oauth, user, worker, payroll).
    Build bem-sucedido das imagens Docker locais (v1).

--- hr-worker/src/main/resources/data.sql
+++ hr-worker/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_worker (name, daily_Income) VALUES ('Bob', 200.0);
-INSERT INTO tb_worker (name, daily_Income) VALUES ('Maria', 300.0);
-INSERT INTO tb_worker (name, daily_Income) VALUES ('Alex', 250.0);
+INSERT INTO tb_worker (name, daily_income) VALUES ('Bob Docker', 200.0);
+INSERT INTO tb_worker (name, daily_income) VALUES ('Maria Docker', 300.0);
+INSERT INTO tb_worker (name, daily_income) VALUES ('Alex Docker', 250.0);
